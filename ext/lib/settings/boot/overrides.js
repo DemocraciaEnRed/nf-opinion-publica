b540a4c1b74386b3c7286b3397a424337c366a4d
Inve <inveniahadouken@gmail.com>
2019-10-02 16:36:06 -0300

    new field in settings

--- ext/lib/settings/boot/overrides.js
+++ ext/lib/settings/boot/overrides.js
@@ -3,5 +3,9 @@ import 'ext/lib/boot/overrides'
 import * as ForumForm from 'lib/settings/forum-new/forum-form/forum-form'
 import ForumFormExt from 'ext/lib/settings/forum-new/forum-form/forum-form'
 
+import * as Settings from 'lib/settings/settings-profile/view.js'
+import SettingsExt from 'ext/lib/settings/settings-profile/view.js'
+
 
 ForumForm.default = ForumFormExt
+Settings.default = SettingsExt
